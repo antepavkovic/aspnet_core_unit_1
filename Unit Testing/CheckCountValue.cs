50a9cf981fe0148caafa3b42f4bde82d57657ff9
antepavkovic <ante.pavkovic357@gmail.com>
2023-08-28 11:08:49 +0200

    Add project files.

---
+++ Unit Testing/CheckCountValue.cs
@@ -0,0 +1,24 @@
+﻿using aspnet_core_unit_1.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Unit_Testing
+{
+   
+    public class CheckCountValue
+    {
+        [Fact]
+        public void If_Else_ConditionCheck_Count_Value()
+        {
+            var controller = new HomeController();
+            Assert.Throws<Exception>(() => controller.CheckCountValue(500)); // should throw
+         
+            controller.CheckCountValue(2); // should not throw
+           
+        }
+    }
+}
